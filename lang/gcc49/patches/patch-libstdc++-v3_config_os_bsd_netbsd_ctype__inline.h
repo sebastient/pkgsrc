7bc3bf010866dbefd68b68f1c86f5758514112f7
Jonathan Perkin <jperkin@joyent.com>
2014-09-04 17:23:00 +0100

    Try out new gcc49 packages.

---
+++ lang/gcc49/patches/patch-libstdc++-v3_config_os_bsd_netbsd_ctype__inline.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Support _CTYPE_BL
+
+--- libstdc++-v3/config/os/bsd/netbsd/ctype_inline.h.orig	2014-01-02 22:30:10.000000000 +0000
++++ libstdc++-v3/config/os/bsd/netbsd/ctype_inline.h
+@@ -48,7 +48,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+   is(const char* __low, const char* __high, mask* __vec) const
+   {
+     while (__low < __high)
+-      *__vec++ = _M_table[*__low++];
++      *__vec++ = _M_table[(unsigned char)*__low++];
+     return __high;
+   }
+ 
